c065106a09a97cdd38b7f6e54c952bd6e9e92b37
Victor Sabino <victorsabino6@gmail.com>
2021-09-19 23:32:17 -0300

    added saved route

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -35,18 +35,34 @@ class RandomWordsState extends State<RandomWords> {
             color: isSaved ? Colors.red : null),
         onTap: () {
           setState(() => {
-            if (isSaved) 
-              _savedPairs.remove(pair) 
-            else 
-              _savedPairs.add(pair)
-          });
+                if (isSaved) _savedPairs.remove(pair) else _savedPairs.add(pair)
+              });
         });
+  }
+
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _savedPairs.map((WordPair pair) {
+        return ListTile(
+          title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18)),
+        );
+      });
+        final List<Widget> divided =
+            ListTile.divideTiles(context: context, tiles: tiles).toList();
+        return Scaffold(
+          appBar: AppBar(title: Text('Saved WordPairs')),
+          body: ListView(children: divided));
+    }));
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('WordPair Genarator'),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
+          ],
           backgroundColor: Colors.green,
         ),
         body: _buildList());
